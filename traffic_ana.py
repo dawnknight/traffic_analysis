27051aa7ad4041dc5a83b531e6f14444e4ad7414
dawnknight <atc327@nyu.edu>
2014-02-02 22:16:32 -0500

    update
    now it works

--- traffic_ana.py
+++ traffic_ana.py
@@ -4,13 +4,15 @@ Created on Mon Jan 27 12:08:00 2014
 
 @author: atc327
 """
-import os, glob,sys
+import os, glob,sys,pylab,pickle
 import scipy as sp
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 from scipy import optimize
-import pylab
+
+f = file('shift.pkl')
+sft = pickle.load(f)
 
     
 #path = '/gpfs1/cusp/andyc/Night/'
@@ -32,23 +34,36 @@ im2 = np.zeros([750,900])
 
 
 for i in range(len(imlist)-dt):
-    print i
-    im1 = np.array(Image.open(imlist[i+dt]).convert('L')).astype(np.float)
-    im2 = np.array(Image.open(imlist[i]).convert('L')).astype(np.float)
- 
-    diff = im1-im2
-    L1_cut = diff[L1[0][1]:L1[1][1],L1[0][0]:L1[1][0]]
-    L2_cut = diff[L2[0][1]:L2[1][1],L2[0][0]:L2[1][0]]
-    car_cut =diff[car[0][1]:car[1][1],car[0][0]:car[1][0]]
-    ped_cut =diff[ped[0][1]:ped[1][1],ped[0][0]:ped[1][0]]
-    L1_var.append(L1_cut.var())
-    L2_var.append(L2_cut.var())
-    car_var.append(car_cut.var())
-    ped_var.append(ped_cut.var())
-figure(5,figsize=[7.5,7.5]),
-plot(range(len(L1_var)),L1_var,color = '#990000',lw=2)
-plot(range(len(L1_var)),L2_var, color = '#006600',lw=2)
-#plot(range(len(L1_var)),ped_var, color = 'black',lw=2)
+      print i
+      im1 = np.array(Image.open(imlist[i+dt]).convert('L')).astype(np.float)
+      im2 = np.array(Image.open(imlist[i]).convert('L')).astype(np.float)
+       
+#        diff = im1-im2
+#        L1_cut = diff[L1[0][1]:L1[1][1],L1[0][0]:L1[1][0]]
+#        L2_cut = diff[L2[0][1]:L2[1][1],L2[0][0]:L2[1][0]]
+#        car_cut =diff[car[0][1]:car[1][1],car[0][0]:car[1][0]]
+#        ped_cut =diff[ped[0][1]:ped[1][1],ped[0][0]:ped[1][0]]
+      if sft[i+dt][1]>100 :
+         sft[i+dt][1] =0 
+      
+      
+      L1_cut = im1[L1[0][1]-sft[i+dt][0]:L1[1][1]-sft[i+dt][0],L1[0][0]-sft[i+dt][1]:L1[1][0]-sft[i+dt][1]]-\
+               im2[L1[0][1]-sft[i][0]:L1[1][1]-sft[i][0],L1[0][0]-sft[i][1]:L1[1][0]-sft[i][1]]
+      L2_cut = im1[L2[0][1]-sft[i+dt][0]:L2[1][1]-sft[i+dt][0],L2[0][0]-sft[i+dt][1]:L2[1][0]-sft[i+dt][1]]-\
+               im2[L2[0][1]-sft[i][0]:L2[1][1]-sft[i][0],L2[0][0]-sft[i][1]:L2[1][0]-sft[i][1]] 
+      car_cut = im1[car[0][1]-sft[i+dt][0]:car[1][1]-sft[i+dt][0],car[0][0]-sft[i+dt][1]:car[1][0]-sft[i+dt][1]]-\
+               im2[car[0][1]-sft[i][0]:car[1][1]-sft[i][0],car[0][0]-sft[i][1]:car[1][0]-sft[i][1]]
+      ped_cut = im1[ped[0][1]-sft[i+dt][0]:ped[1][1]-sft[i+dt][0],ped[0][0]-sft[i+dt][1]:ped[1][0]-sft[i+dt][1]]-\
+               im2[ped[0][1]-sft[i][0]:ped[1][1]-sft[i][0],ped[0][0]-sft[i][1]:ped[1][0]-sft[i][1]] 
+      L1_var.append(L1_cut.var())
+      L2_var.append(L2_cut.var())
+      car_var.append(car_cut.var())
+      ped_var.append(ped_cut.var()) 
+              
+plt.figure(1,figsize=[7.5,7.5]),
+plt.plot(range(len(L1_var)),L1_var,color = '#990000',lw=2)
+plt.plot(range(len(L1_var)),L2_var, color = '#006600',lw=2)
+#plt.plot(range(len(L1_var)),ped_var, color = 'black',lw=2)
 fill_between(range(len(L1_var)),car_var,facecolor = '#0099FF',edgecolor='#0000FF')
 
 plt.grid(b=1,lw =2)
@@ -62,7 +77,7 @@ pylab.xlim([500,674])
 #plt.text(145,420, 'traffic light region 1',color = '#006600',fontsize = 12)
 #plt.text(145,435, 'traffic light region 2',color = '#990000',fontsize = 12)
 #
-ticks = np.arange(0, len(imlist), len(imlist)/27)
-labels = range(ticks.size)
-plt.xticks(ticks[20:], labels[20:])
+#ticks = np.arange(0, len(imlist), len(imlist)/27)
+#labels = range(ticks.size)
+#plt.xticks(ticks[20:], labels[20:])
 
